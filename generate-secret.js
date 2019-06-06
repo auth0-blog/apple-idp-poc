e636241d20396d8cfbd18968fc629aca09ad267f
Bruno Krebs <bruno.krebs@auth0.com>
2019-06-06 17:45:31 +1200

    webapp;

--- generate-secret.js
+++ generate-secret.js
@@ -7,7 +7,7 @@ const token = jwt.sign({}, privateKey, {
     expiresIn: '2 days',
     audience: 'https://appleid.apple.com',
     issuer: '936A789GM7',
-    subject: 'com.brunokrebs.appid'
+    subject: 'com.brunokrebs.webapp'
 });
 
 console.log('The token is:', token);
